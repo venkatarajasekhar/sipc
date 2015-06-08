3a3ce97a0a767c18297ac3f87e5000436cfc9694
Steven Wright <steven.wright@adroll.com>
2015-06-08 09:25:24 -0700

    Consistent return values

--- sipc_conn.c
+++ sipc_conn.c
@@ -5,27 +5,29 @@
 inline static int read_acquire_attempt(conn_t conn)
 {
     conn_header_t * conn_header = (conn_header_t *) conn.in_buffer;
-    return __sync_val_compare_and_swap(&conn_header->state,
-                                       IDLE,
-                                       READ_LOCKED);
+    return !__sync_bool_compare_and_swap(&conn_header->state,
+                                         IDLE,
+                                         READ_LOCKED);
 }
 
 inline static int read_acquire(conn_t conn)
 {
     while(!read_acquire_attempt(conn));
+    return 0;
 }
 
 inline static int write_acquire_attempt(conn_t conn)
 {
     conn_header_t * conn_header = (conn_header_t *) conn.out_buffer;
-    return __sync_val_compare_and_swap(&conn_header->state,
-                                       IDLE,
-                                       WRITE_LOCKED);
+    return !__sync_bool_compare_and_swap(&conn_header->state,
+                                         IDLE,
+                                         WRITE_LOCKED);
 }
 
 inline static int write_acquire(conn_t conn)
 {
     while(!read_acquire_attempt(conn));
+    return 0;
 }
 
 int recv_unsafe_acquire(conn_t conn,
@@ -52,9 +54,9 @@ int recv_unsafe_acquire(conn_t conn,
 int recv_unsafe_release(conn_t conn)
 {
     conn_header_t * conn_header = (conn_header_t*) conn.in_buffer;
-    return __sync_val_compare_and_swap(&conn_header->state,
-                                       READ_LOCKED,
-                                       IDLE);
+    return !__sync_bool_compare_and_swap(&conn_header->state,
+                                        READ_LOCKED,
+                                        IDLE);
 }
 
 int send_unsafe_acquire(conn_t conn,
@@ -71,18 +73,19 @@ int send_unsafe_acquire(conn_t conn,
     {
         if(!read_acquire_attempt(conn))
         {
-            return 0;
+            return 1;
         }
     }
 
     *buffer = conn_header->data;
     *max_len = conn_header->max_len;
-    return 1;
+    return 0;
 }
+
 int send_unsafe_release(conn_t conn)
 {
     conn_header_t * conn_header = (conn_header_t*) conn.out_buffer;
-    return __sync_val_compare_and_swap(&conn_header->state,
-                                       WRITE_LOCKED,
-                                       IDLE);
+    return !__sync_bool_compare_and_swap(&conn_header->state,
+                                         WRITE_LOCKED,
+                                         IDLE);
 }
