e9ddb4e55e576b623df85c62565e34a41c8368f4
Steven Wright <steven.wright@adroll.com>
2015-06-12 20:12:33 -0700

    Consistent styling

--- sipc_conn.c
+++ sipc_conn.c
@@ -34,7 +34,7 @@ int recv_unsafe_acquire(conn_t conn,
                         void  ** buffer,
                         blocking_behavior_t behavior)
 {
-    conn_header_t * conn_header = (conn_header_t * ) conn.in_buffer;
+    conn_header_t * conn_header = (conn_header_t *) conn.in_buffer;
     if(behavior == BLOCKING)
     {
         read_acquire(conn);
@@ -53,7 +53,7 @@ int recv_unsafe_acquire(conn_t conn,
 
 int recv_unsafe_release(conn_t conn)
 {
-    conn_header_t * conn_header = (conn_header_t*) conn.in_buffer;
+    conn_header_t * conn_header = (conn_header_t *) conn.in_buffer;
     return !__sync_bool_compare_and_swap(&conn_header->state,
                                         READ_LOCKED,
                                         IDLE);
@@ -84,7 +84,7 @@ int send_unsafe_acquire(conn_t conn,
 
 int send_unsafe_release(conn_t conn)
 {
-    conn_header_t * conn_header = (conn_header_t*) conn.out_buffer;
+    conn_header_t * conn_header = (conn_header_t *) conn.out_buffer;
     return !__sync_bool_compare_and_swap(&conn_header->state,
                                          WRITE_LOCKED,
                                          IDLE);
